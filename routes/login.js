0ebf1ac361f4caa432f074a5e05c7f30ebe18d43
Miranda Zheng <zhengshuang2011@gmail.com>
2022-01-10 03:28:04 +0000

    Delete the login.ejs and updated the router for login

---
+++ routes/login.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+module.exports = (db) => {
+  // router.get("/", (req, res) => {
+  //   db.query(`SELECT * FROM users;`)
+  //     .then((data) => {
+  //       const users = data.rows;
+  //       res.json({ users });
+  //     })
+  //     .catch((err) => {
+  //       res.status(500).json({ error: err.message });
+  //     });
+  // });
+
+  const login = (email, password) => {
+    return db.getUserWithEmail(email).then((user) => {
+      if (bcrypt.compareSync(password, user.password)) {
+        return user;
+      }
+      return null;
+    });
+  };
+  exports.login = login;
+
+  router.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    login(email, password)
+      .then((user) => {
+        if (!user) {
+          res.send({ error: "no user found, please register first" });
+          return;
+        }
+        req.session.userId = user.id;
+        res.send({ user: { name: user.name, email: user.email, id: user.id } });
+      })
+      .catch((e) => res.send(e));
+  });
+
+  return router;
+};
--- server.js
+++ server.js
@@ -37,11 +37,15 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const login = require("./routes/login");
+// const profileEditRoutes = require("./routes/profileEdit");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("./api/login", login(db));
+// app.use("/api/profileEdit", profileEditRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
