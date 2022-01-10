0ff30159c454e4d95bcdd8e58b608713d588c50d
Miranda Zheng <zhengshuang2011@gmail.com>
2022-01-10 15:49:49 +0000

    Edit some sql queries

--- db/seeds/03_todo_lists.sql
+++ db/seeds/03_todo_lists.sql
@@ -1,7 +1,7 @@
-INSERT INTO todos (user_id, category_id, [name])
+INSERT INTO todos (user_id, category_id, name)
  VALUES (1, 2, 'Lord of the Rings'),
         (1, 3, 'The Rise'),
         (1, 1, 'Bananas'),
         (1, 4, 'newspaper'),
         (1, 1, 'Tim Hortons'),
-        (1, 'space');
+        (1, null,'space');
